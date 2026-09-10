e8ba1b33b0ab5fbb98f40c7b45fb46e51eabac6d
parbol <pablom@cern.ch>
2026-09-10 12:13:40 +0200

    Updated scenario with the actual BTL numbers

--- Alignment/MTDAlignment/python/Scenarios_cff.py
+++ Alignment/MTDAlignment/python/Scenarios_cff.py
@@ -32,41 +32,89 @@ MTDBTLStartup = cms.PSet(
         scale = cms.double(1.0),
         scaleError = cms.double(1.0),
         dZ = cms.double(0.0),
-        dX = cms.double(0.1),
+        dX = cms.double(0.0),
         dY = cms.double(0.0),
         phiZ = cms.double(0.000),
         phiY = cms.double(0.000),
         phiX = cms.double(0.000),
         BTLTrays = cms.PSet(
             dZ = cms.double(0.1),
-            dX = cms.double(0.2),
+            dX = cms.double(0.035),
             dY = cms.double(0.0),
             phiZ = cms.double(0.000),
             phiY = cms.double(0.000),
             phiX = cms.double(0.000),
             BTLRUs = cms.PSet(
                 dZ = cms.double(0.0),
-                dX = cms.double(0.3),
-                dY = cms.double(0.2),
+                dX = cms.double(0.0),
+                dY = cms.double(0.0),
                 phiZ = cms.double(0.000),
                 phiY = cms.double(0.000),
                 phiX = cms.double(0.000),
                 BTLModules = cms.PSet(
                     dZ = cms.double(0.0),
-                    dX = cms.double(0.4),
+                    dX = cms.double(0.015),
                     dY = cms.double(0.015),
                     phiZ = cms.double(0.000),
                     phiY = cms.double(0.000),
                     phiX = cms.double(0.000),
                     BTLSensorModules = cms.PSet(
                          dZ = cms.double(0.0),
-                         dX = cms.double(0.5),
-                         dY = cms.double(0.015),
+                         dX = cms.double(0.0),
+                         dY = cms.double(0.0),
                          phiZ = cms.double(0.000),
                          phiY = cms.double(0.000),
                          phiX = cms.double(0.000)
                     )
                 )
+            ),
+            BTLRU1 = cms.PSet(
+                dZ = cms.double(0.0),
+                dX = cms.double(0.0),
+                dY = cms.double(0.2),
+                phiZ = cms.double(0.000),
+                phiY = cms.double(0.000),
+                phiX = cms.double(0.000)
+            ),
+            BTLRU2 = cms.PSet(
+                dZ = cms.double(0.0),
+                dX = cms.double(0.0),
+                dY = cms.double(0.164),
+                phiZ = cms.double(0.000),
+                phiY = cms.double(0.000),
+                phiX = cms.double(0.000)
+            ),
+            BTLRU3 = cms.PSet(
+                dZ = cms.double(0.0),
+                dX = cms.double(0.0),
+                dY = cms.double(0.127),
+                phiZ = cms.double(0.000),
+                phiY = cms.double(0.000),
+                phiX = cms.double(0.000)
+            ),
+            BTLRU4 = cms.PSet(
+                dZ = cms.double(0.0),
+                dX = cms.double(0.0),
+                dY = cms.double(0.091),
+                phiZ = cms.double(0.000),
+                phiY = cms.double(0.000),
+                phiX = cms.double(0.000)
+            ),
+            BTLRU5 = cms.PSet(
+                dZ = cms.double(0.0),
+                dX = cms.double(0.0),
+                dY = cms.double(0.054),
+                phiZ = cms.double(0.000),
+                phiY = cms.double(0.000),
+                phiX = cms.double(0.000)
+            ),
+            BTLRU6 = cms.PSet(
+                dZ = cms.double(0.0),
+                dX = cms.double(0.0),
+                dY = cms.double(0.0),
+                phiZ = cms.double(0.000),
+                phiY = cms.double(0.000),
+                phiX = cms.double(0.000)
             )
         )
     ),
